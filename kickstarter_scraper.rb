484fea58270636f58116a4c8026b89dc75b86200
arnie b <arnold.bwaila@gmail.com>
2020-01-16 00:37:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # project.css("p.bbcard_blurb").text
-
+#project.css("ul.project-stats li.first.funded strong").text
 #project.css("span.location-name").text
 def create_project_hash
   # write your code here
